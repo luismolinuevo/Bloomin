817b2db5c77044d01b9e0ebe2450cd0f944de151
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-10-29 04:49:33 -0400

    added favorites to router

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,13 @@ import { Router } from "express";
 import authRoutes from "./auth.js";
 import postRoutes from "./post.js";
 import commentRoutes from "./comment.js";
+import favoritesRoutes from "./favorites.js";
 
 const apiRouter = Router();
 
 apiRouter.use(authRoutes);
 apiRouter.use(postRoutes);
 apiRouter.use(commentRoutes);
+apiRouter.use(favoritesRoutes);
 
 export { apiRouter }
