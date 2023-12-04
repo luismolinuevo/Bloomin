d7e99992c92cda1223564286ac088ba65ed1e3fd
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-12-04 00:44:39 -0500

    init form and signup page done

--- client/app/(auth)/signup/Form.jsx
+++ client/app/(auth)/signup/Form.jsx
@@ -11,30 +11,49 @@ export default function Form() {
     formState: { errors },
   } = useForm();
   return (
-    <div className="flex justify-center items-center h-full">
-      <form onSubmit={handleSubmit(data)} className="flex flex-col w-1/6">
-        <input
-          {...register("email", { required: true })}
-          className="bg-white p-1"
-        />
-        {errors.email && <p>Email is required.</p>}
-        <input
-          {...register("Username", { required: true })}
-          className="bg-white p-1"
-        />
-        {errors.Username && <p>Username is required.</p>}
-        <input
-          {...register("Password", { required: true })}
-          className="bg-white p-1"
-        />
-        {errors.Password && <p>Password is required.</p>}
-        <input
-          {...register("ConfirmPassword", { required: true })}
-          className="bg-white p-1"
-        />
-        {errors.ConfirmPassword && <p>Confirm Password is required.</p>}
-        <input type="submit" />
-      </form>
+    <div className="flex justify-center items-center">
+      <div className="w-[350px]">
+        <form
+          onSubmit={handleSubmit(data)}
+          className="flex flex-col gap-7"
+        >
+          <input
+            {...register("email", { required: true })}
+            className="bg-white p-1 rounded-lg h-[50px]"
+            placeholder="Email"
+          />
+          {errors.email && <p>Email is required.</p>}
+          <input
+            {...register("Username", { required: true })}
+            className="bg-white p-1 rounded-lg h-[50px]"
+            placeholder="Username"
+          />
+          {errors.Username && <p>Username is required.</p>}
+          <input
+            {...register("Password", { required: true })}
+            className="bg-white p-1 rounded-lg h-[50px]"
+            placeholder="Password"
+          />
+          {errors.Password && <p>Password is required.</p>}
+          <input
+            {...register("ConfirmPassword", { required: true })}
+            className="bg-white p-1 rounded-lg h-[50px]"
+            placeholder="Confirm Password"
+          />
+          {errors.ConfirmPassword && <p>Confirm Password is required.</p>}
+          <div className="flex justify-center mt-5">
+            <input
+              type="submit"
+              className="py-1 px-2 bg-green-3 text-white w-1/2 rounded-[50px] text-[28px]"
+            />
+          </div>
+        </form>
+        <div className="flex items-center gap-2 my-5">
+          <hr className="h-1 border-black w-[100px]" />
+          <p>or Sign up with</p>
+          <hr className="h-1 border-black w-[100px]" />
+        </div>
+      </div>
     </div>
   );
 }
--- client/app/(auth)/signup/page.jsx
+++ client/app/(auth)/signup/page.jsx
@@ -3,10 +3,12 @@ import Form from "./Form";
 
 export default function index() {
   return (
-      <div className="h-screen w-full bg-[#E5EBE1]">
-        <h1>Sign Up</h1>
+    <div className="h-screen w-full bg-[#E5EBE1] flex items-center justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-center text-[38px] mb-12 text-bold">Sign Up</h1>
         <Form />
       </div>
+    </div>
   );
 }
 //need layout to have navbar on top
