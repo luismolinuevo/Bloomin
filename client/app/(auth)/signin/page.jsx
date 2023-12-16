44c37c0c44050a7ae9ce2a42825bac48727145dd
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-12-16 00:03:07 -0500

    signup form created

---
+++ client/app/(auth)/signin/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Form from "./Form";
+import Google from "@/app/components/Auth/Google";
+
+export default function page() {
+  return (
+    <div className="h-screen w-full bg-[#E5EBE1] flex items-center justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-center text-[38px] mb-12 text-bold">Sign Up</h1>
+        <Form />
+        <Google />
+      </div>
+    </div>
+  );
+}
---
+++ client/app/utils/clearUrl.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import cookie from "js-cookie";
+
+export default function useClearUrl() {
+
+}
