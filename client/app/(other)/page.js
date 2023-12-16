7dcabdc742615c6be5faee9a6e0133dbced13730
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-12-16 01:48:25 -0500

    clear the url when using google oauth

--- client/app/(other)/page.js
+++ client/app/(other)/page.js
@@ -1,3 +1,8 @@
+"use client"
+
+import useClearUrl from "../utils/clearUrl";
+
 export default function Home() {
+  useClearUrl();
   return <div></div>;
 }
--- client/app/utils/clearUrl.js
+++ client/app/utils/clearUrl.js
@@ -1,9 +1,23 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import cookie from "js-cookie";
 
 export default function useClearUrl() {
+  const router = useRouter();
 
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const tokenParam = url.searchParams.get("token");
+
+    if (tokenParam) {
+      cookie.set("user_token", tokenParam);
+
+      const newUrl = window.location.href.split("?")[0];
+      window.history.replaceState({}, document.title, newUrl);
+
+      router.push("/");
+    }
+  }, [router]);
 }
