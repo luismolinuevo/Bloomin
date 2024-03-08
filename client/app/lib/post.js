25664cbda7e8ae0ccff353bdf60a9d04ff69bfd7
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-03-07 19:32:48 -0500

    called vote post api on frontend

--- client/app/lib/post.js
+++ client/app/lib/post.js
@@ -51,6 +51,40 @@ export const createPost = async (data, token) => {
     return responseData;
   } catch (error) {
     // Log more details about the error
+    console.error("Error during creating post:", error);
+    throw error;
+  }
+};
+
+export const votePost = async (post_id, token) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/vote/${post_id}`,
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      console.error(
+        "Vote post request request failed. Response status:",
+        response.status
+      );
+      const errorData = await response.json();
+      console.error("Error details:", errorData);
+
+      throw new Error("Vote request failed");
+    }
+
+    // If the response is successful, return the data (if needed)
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    // Log more details about the error
     console.error("Error during signup:", error);
     throw error;
   }
