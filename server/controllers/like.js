9b959fa7a04efe7a6511bad42fa1ab59c90d893c
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-03-15 01:36:20 -0400

    fixed create comment, created comment reply stock

--- server/controllers/like.js
+++ server/controllers/like.js
@@ -72,15 +72,15 @@ const commentLike = async (req, res) => {
   const { type } = req.body;
 
   try {
-    //Getting the post
-    const post = await prisma.post.findFirst({
+    //Getting the comment
+    const comment = await prisma.comment.findFirst({
       where: {
-        id: parseInt(post_id),
+        id: parseInt(comment_id),
       },
     });
 
-    //If there is a post with the post_id
-    if (post) {
+    //If there is a comment with comment id
+    if (comment) {
       //See if like exist with the post and user id
       const existingLike = await prisma.like.findFirst({
         where: {
@@ -120,7 +120,7 @@ const commentLike = async (req, res) => {
       }
     } else {
       return res.status(404).json({
-        message: "No post with that id found. Make sure id is valid",
+        message: "No comment with that id found. Make sure id is valid",
         success: false,
       });
     }
@@ -134,6 +134,71 @@ const commentLike = async (req, res) => {
   }
 };
 
-const commentReplyLike = async (req, res) => {};
+const commentReplyLike = async (req, res) => {
+  const { comment_id } = req.params;
+  const { type } = req.body;
 
-export { postLike, commentLike };
+  try {
+    //Getting the comment rely
+    const comment = await prisma.commentReply.findFirst({
+      where: {
+        id: parseInt(comment_id),
+      },
+    });
+
+    //If there is a comment with comment id
+    if (comment) {
+      //See if like exist with the post and user id
+      const existingLike = await prisma.like.findFirst({
+        where: {
+          commentReply: parseInt(comment_id),
+          userId: req.user.id,
+        },
+      });
+
+      //If exisitingLike
+      if (existingLike) {
+        return res.status(400).json({
+          message: "You have already liked this comment",
+          success: false,
+        });
+        //Create
+      } else {
+        const likeComment = await prisma.like.create({
+          data: {
+            type: type,
+            commentReply: parseInt(comment_id),
+            userId: req.user.id,
+          },
+        });
+
+        if (likeComment) {
+          return res.status(200).json({
+            message: "Liked comment",
+            success: true,
+            likeComment,
+          });
+        } else {
+          return res.status(500).json({
+            message: "Error creating like",
+            success: false,
+          });
+        }
+      }
+    } else {
+      return res.status(404).json({
+        message: "No comment with that id found. Make sure id is valid",
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: error,
+    });
+  }
+};
+
+export { postLike, commentLike, commentReplyLike };
--- server/routes/like.js
+++ server/routes/like.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
-import * as likeControllers from "../controllers/like.js"
+import * as likeControllers from "../controllers/like.js";
 import passport from "passport";
 
 const router = Router();
 
-router.post("/postlike/:post_id", passport.authenticate("jwt", { session: false }), likeControllers.postLike);
-router.post("/commentlike/:post_id", passport.authenticate("jwt", { session: false }), likeControllers.commentLike);
+router.post(
+  "/postlike/:post_id",
+  passport.authenticate("jwt", { session: false }),
+  likeControllers.postLike
+);
+router.post(
+  "/commentlike/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  likeControllers.commentLike
+);
+router.post(
+  "/commentreplylike/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  likeControllers.commentReplyLike
+);
 
 export default router;
