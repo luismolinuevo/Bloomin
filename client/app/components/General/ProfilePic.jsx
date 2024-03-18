091ec0844aba9fa3926d96c1abe27e79b9526d4c
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-03-18 01:51:53 -0400

    added default img for profile, may need a better one in future

--- client/app/components/General/ProfilePic.jsx
+++ client/app/components/General/ProfilePic.jsx
@@ -1,7 +1,14 @@
-"use client"
+"use client";
 
 import { Avatar } from "@material-tailwind/react";
- 
-export function AvatarDefault({src}) {
-  return <Avatar src={src} alt="avatar" size="md" title="Profile Picture"/>;
+
+export function AvatarDefault({ src }) {
+  return (
+    <Avatar
+      src={src ? src : "https://docs.material-tailwind.com/img/face-2.jpg"}
+      alt="avatar"
+      size="md"
+      title="Profile Picture"
+    />
+  );
 }
