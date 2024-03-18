99dd09b2bde2a5ae7ee7c9fb6c83fc70a1ef5755
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-03-17 23:02:56 -0400

    auth reducers

--- client/app/store/slices/auth.js
+++ client/app/store/slices/auth.js
@@ -2,11 +2,21 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const Auth = createSlice({
   name: "auth",
-  initialState: {},
+  initialState: {
+    isLoggedIn: true,
+    userData: null,
+  },
 
-  reducers: {},
+  reducers: {
+    setIsLoggedIn: (state, action) => {
+      state.isLoggedIn = action.payload.isLoggedIn;
+    },
+    setUserData: (state, action) => {
+      state.userData = action.payload.userData;
+    },
+  },
 });
 
-export const {} = Auth.actions;
+export const { setIsLoggedIn, setUserData } = Auth.actions;
 
 export default Auth.reducer;
