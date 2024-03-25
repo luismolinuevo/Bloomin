7b96e5a55b9e0b7eedea1303c4735168424721c0
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-03-25 19:01:23 -0400

    code clean up

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -28,97 +28,9 @@ const createPost = async (req, res) => {
   }
 };
 
-const vote = async (req, res) => {
-  const { postId } = req.params;
-  const type = req.query.type;
-  console.log(postId);
-  console.log(type);
-
-  try {
-    const post = await prisma.post.findUnique({
-      where: { id: parseInt(postId) },
-    });
-
-    if (!post) {
-      return res.status(404).json({ error: "Post not found" });
-    }
-
-    const existingVote = await prisma.votes.findFirst({
-      where: {
-        postId: parseInt(postId),
-        userId: req.user.id,
-      },
-    });
-
-    if (existingVote) {
-      if (existingVote.type === type) {
-        return res.status(200).json({
-          error: `You have already ${type}voted this post`,
-          success: true,
-        });
-      } else {
-        // Decrement the corresponding vote count if it's greater than 0
-        if (existingVote.type === "upvote" && post.upvotes > 0) {
-          await prisma.post.update({
-            where: { id: parseInt(postId) },
-            data: {
-              upvotes: { decrement: 1 },
-            },
-          });
-        } else if (existingVote.type === "downvote" && post.downvotes > 0) {
-          await prisma.post.update({
-            where: { id: parseInt(postId) },
-            data: {
-              downvotes: { decrement: 1 },
-            },
-          });
-        }
-
-        await prisma.votes.delete({
-          where: { id: existingVote.id },
-        });
-      }
-    }
-
-    // Increment the corresponding vote count
-    if (type === "upvote") {
-      await prisma.post.update({
-        where: { id: parseInt(postId) },
-        data: { upvotes: { increment: 1 } },
-      });
-    } else if (type === "downvote") {
-      await prisma.post.update({
-        where: { id: parseInt(postId) },
-        data: { downvotes: { increment: 1 } },
-      });
-    }
-
-    await prisma.votes.create({
-      data: {
-        user: { connect: { id: req.user.id } },
-        type,
-        post: { connect: { id: parseInt(postId) } },
-      },
-    });
-
-    const updatedVotes = await prisma.post.findUnique({
-      where: { id: parseInt(postId) },
-    });
-
-    return res.status(200).json({
-      message: `Successfully ${type}voted the post`,
-      success: true,
-      updatedVotes,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Something went wrong" });
-  }
-};
-
 const getAllPost = async (req, res) => {
   try {
-    const { cursor, limit = 3 } = req.query;
+    const { cursor, limit = 15 } = req.query;
 
     // Retrieve posts with associated user, applying pagination
     let posts;
