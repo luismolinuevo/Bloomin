45e3cce185ba166a8bff00e5013cd5354beb3103
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-03-26 17:56:54 -0400

    fixed how I handle fav on client

--- client/app/components/Post/FavoritePostButton.jsx
+++ client/app/components/Post/FavoritePostButton.jsx
@@ -6,10 +6,24 @@ import { useAppSelector } from "@/app/store/reduxhooks";
 
 export default function FavoritePostButton({ post, token }) {
   const [faved, setFaved] = useState(post?.userFav); //doing it this way so api doest have to refresh, I would also use web sockets.
+  const [favLikes, setFavLikes] = useState(post?.favCount);
   const handleFavorite = async () => {
-    const data = await favoritePost(post?.id, token);
-    console.log(data);
-    setFaved(!faved);
+    try {
+      const data = await favoritePost(post?.id, token);
+      console.log(data);
+      //true
+      if (data.success) {
+        if (faved) {
+          setFaved(false);
+          setFavLikes(favLikes - 1);
+        } else {
+          setFaved(true);
+          setFavLikes(favLikes + 1);
+        }
+      }
+    } catch (error) {
+      console.log("There has been a error faving the post ", error);
+    }
   };
 
   return (
@@ -35,7 +49,7 @@ export default function FavoritePostButton({ post, token }) {
             d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
           />
         </svg>
-        <p>{post?.favCount}</p>
+        <p>{favLikes}</p>
       </button>
     </div>
   );
