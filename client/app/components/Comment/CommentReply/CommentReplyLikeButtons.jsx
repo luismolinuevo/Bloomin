e8cfc5c944c89eaad2f4fff7f7c9ccac7945fecb
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-03-27 15:44:30 -0400

    show comment like count on client

--- client/app/components/Comment/CommentReply/CommentReplyLikeButtons.jsx
+++ client/app/components/Comment/CommentReply/CommentReplyLikeButtons.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import { likeComment } from "@/app/lib/commentreply";
 
-export default function CommentReplyLikeButtons({
-  token,
-  comment,
-  setRefresh,
-  refresh,
-}) {
+export default function CommentReplyLikeButtons({ token, comment }) {
+  const [liked, setLiked] = useState(comment?.userLike);
+  const [likeCount, setLikeCount] = useState(comment?.replyLikeCount);
+  console.log(comment);
+  
   const onClick = async (type) => {
     try {
       const data = {
@@ -14,31 +15,42 @@ export default function CommentReplyLikeButtons({
       };
       const like = await likeComment(comment?.id, data, token);
       if (like.success) {
-        //do something if needed
-        setRefresh(!refresh);
+        if (type === "like") {
+          setLiked("like");
+          setLikeCount(likeCount + 1);
+        } else if (type === "dislike") {
+          setLiked("dislike");
+          setLikeCount(likeCount - 1);
+        }
       }
     } catch (error) {
       console.log("Error liking comments");
     }
   };
   return (
-    <div>
-      <button onClick={() => onClick("like")}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z"
-          />
-        </svg>
-      </button>
+    <div className={`flex items-center gap-2`}>
+      <div className={`flex gap-1 `}>
+        <button onClick={() => onClick("like")}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={`w-6 h-6 ${
+              liked === "like" ? "text-green-500" : "text-black"
+            }`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z"
+            />
+          </svg>
+        </button>
+        <p>{likeCount}</p>
+      </div>
+
       <button onClick={() => onClick("dislike")}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -46,7 +58,9 @@ export default function CommentReplyLikeButtons({
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6"
+          className={`w-6 h-6 ${
+            liked === "dislike" ? "text-green-500" : "text-black"
+          }`}
         >
           <path
             strokeLinecap="round"
