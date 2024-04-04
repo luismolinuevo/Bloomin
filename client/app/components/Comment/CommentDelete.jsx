a8789761860f30a770a400980801b56366770628
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-04-04 15:41:12 -0400

    updated comment menu

---
+++ client/app/components/Comment/CommentDelete.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import Modal from "../General/Modal";
+import { deletePost } from "@/app/lib/post";
+
+export default function DeleteComment({ onClose, isVisable, comment_id, token }) {
+  const handleDelete = async () => {
+    try {
+      const postdelete = await deletePost(post_id, token);
+      if (postdelete.success) {
+        //have toast are something
+      } else {
+      }
+      onClose();
+    } catch (error) {
+      console.log("There ha been a error deleting post");
+    }
+  };
+  
+  return (
+    <div>
+      <Modal onClose={onClose} isVisable={isVisable}>
+        <div className="text-center">
+          <h1 className="text-[25px]">
+            Are you sure you want to delete this post?
+          </h1>
+          <div className="flex gap-3 justify-center mt-2">
+            <button
+              onClick={handleDelete}
+              className="bg-[#459858] px-4 rounded-lg text-white h-[40px]"
+            >
+              Yes
+            </button>
+            <button
+              onClick={onClose}
+              className="bg-red-600 px-4 rounded-lg text-white h-[40px]"
+            >
+              No
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+}
--- client/app/components/Comment/CommentMenu.jsx
+++ client/app/components/Comment/CommentMenu.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+"use client";
 
-export default function CommentMenu({ comment_id }) {
+import React, { useState, useEffect } from "react";
+import {
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+} from "../../utils/MaterialTailwind";
+
+export default function CommentMenu({ comment, token }) {
+  const [confirmDelete, setConfirmDelete] = useState(false);
+
   return (
     <div>
-      <button className="">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-8 h-8"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-          />
-        </svg>
-      </button>
+      <Menu>
+        <MenuHandler>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+            />
+          </svg>
+        </MenuHandler>
+        <MenuList>
+          <MenuItem>
+            <button onClick={() => setConfirmDelete(!confirmDelete)}>
+              Delete
+            </button>
+          </MenuItem>
+        </MenuList>
+      </Menu>
+
+      {/* {confirmDelete && (
+        <DeletePost
+          post_id={post?.id}
+          isVisable={confirmDelete}
+          onClose={() => setConfirmDelete(false)}
+          token={token}
+        />
+      )} */}
     </div>
   );
 }
