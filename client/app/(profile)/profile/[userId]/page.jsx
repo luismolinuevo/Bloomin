0c275e21205d35660429093c1efa9067d259c8a4
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-04-11 03:12:51 -0400

    improved styles, some components that are needed

---
+++ client/app/(profile)/profile/[userId]/page.jsx
@@ -0,0 +1,10 @@
+import ProfileHeader from "@/app/components/Profile/ProfileHeader";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <ProfileHeader />
+    </div>
+  );
+}
--- client/app/components/Profile/ProfileHeader.jsx
+++ client/app/components/Profile/ProfileHeader.jsx
@@ -4,19 +4,21 @@ import { AvatarDefault } from "../General/ProfilePic";
 export default function ProfileHeader({ user }) {
   return (
     <div>
-      <div className="flex">
+      <div className="flex justify-center items-center">
         <div className="pr-5">
           <AvatarDefault src={""} size={"xxl"} />
         </div>
-        <div className="flex gap-4">
-          <p>Username</p>
-          <button>EditProfile</button>
-          <p>Setting</p>
-        </div>
-        <div className="flex gap-4">
-          <p>10 Posts</p>
-          <p>1000 Following</p>
-          <p>1000 Followers</p>
+        <div className="flex flex-col gap-3">
+          <div className="flex gap-4">
+            <p className="text-[25px]">Username</p>
+            <button>EditProfile</button>
+            <p>Setting</p>
+          </div>
+          <div className="flex gap-4 text-[17px]">
+            <p>10 Posts</p>
+            <p>1000 Following</p>
+            <p>1000 Followers</p>
+          </div>
         </div>
       </div>
     </div>
