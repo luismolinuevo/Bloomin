47ffe34ea26fdc1c5af4096c9b0926ae5048c7d3
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-04-15 13:41:03 -0400

    some more logic for usercard

--- client/app/components/General/UserCard.jsx
+++ client/app/components/General/UserCard.jsx
@@ -1,16 +1,21 @@
 import { Avatar } from "@material-tailwind/react";
 import React from "react";
 import FollowButton from "../Profile/FollowButton";
+import Link from "next/link";
 
 //This is the card using in components like following popup, and follower popup
-export default function UserCard(user) {
+export default function UserCard(user, token, userFollowing) {
   return (
-    <div className="flex gap-28 items-center">
+    <Link href={`/profile/${user?.id}`} className="flex gap-28 items-center">
       <div className="flex gap-2 items-center">
         <Avatar />
         <h1>Username</h1>
       </div>
-      <FollowButton />
-    </div>
+      <FollowButton
+        token={token}
+        user_id={user}
+        userFollowing={userFollowing}
+      />
+    </Link>
   );
 }
