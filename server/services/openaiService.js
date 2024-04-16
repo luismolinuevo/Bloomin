f6f59e19c87423d961fe3702a79989b3f7f507d2
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-04-16 15:41:25 -0400

    created open ai service

---
+++ server/services/openaiService.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function generateResponse(situation) {
+  // Validate input (if needed)
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(situation),
+      temperature: 0.9,
+      max_tokens: 250,
+    });
+    return completion.data.choices[0].text;
+  } catch (error) {
+    throw error;
+  }
+}
+
+function generatePrompt(situation) {
+  return `You give advice on environmentally conscious things you can do to your home or place of living.
+You respond with a max of 250 tokens, so you should keep your bullets straight to the point.
+Don't give an intro or ending, just respond with 10 bullet points of things to do to be more environmentally conscious based on the following input:
+
+${situation}`;
+}
